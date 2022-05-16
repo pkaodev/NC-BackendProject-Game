5437af8e642dc01c31a9783f2ea234f7f23d1597
pkaodev <polsson.development@gmail.com>
2022-05-16 21:29:30 +0100

    added test for empty object, removed digit checker function from model and replaced with pg error handler, fixed sql query so it updated db correctly

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -17,13 +17,6 @@ exports.fetchReviewById = ({review_id}) => {
 
 //#5
 updateReviewVotes = ({review_id}, {inc_votes}) => {
-    
-    //reject promise if :review_id is a non-number
-    // if (typeof inc_votes !== 'number') {
-    //     return Promise.reject({status: 400, msg: 'Invalid Vote Format'});
-    // }
-
-    // return db.query('SELECT * FROM reviews WHERE review_id = $1', [review_id])
    
     return db.query('UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *', [inc_votes, review_id])
     .then(result => {
