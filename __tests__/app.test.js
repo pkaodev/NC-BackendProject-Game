d96aa6f2f0dcd4f57b4ae412a07770e8f9c32197
pkaodev <polsson.development@gmail.com>
2022-05-17 12:33:15 +0100

    required in jest-sorted and commented some things

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = "test";
 
 const request = require('supertest')
+require('jest-sorted')
 
 
 const db = require('../db/connection')
--- models/reviews.model.js
+++ models/reviews.model.js
@@ -1,4 +1,3 @@
-const { result } = require('lodash');
 const db = require('../db/connection')
 
 //#4
@@ -18,15 +17,16 @@ exports.fetchReviewById = ({review_id}) => {
     })
 }
 
+//#?? gets comments for given review - implement into #4 (fetchReviewById OR CONTROLLER??) and #7
+// exports.fetchCommentCount = (review_id) => {
+//     return db.query('SELECT * FROM comments WHERE review_id = $1', [review_id])
+//     .then(result => {
+//         console.log(result.rows)
+//         return result.rows[0]
+//     })
+// }
+
 //#5
 updateReviewVotes = ({review_id}, {inc_votes}) => {
     return db.query('UPDATE reviews SET votes = votes + $1 WHERE review_id = $2;', [inc_votes, review_id]);
 }
-//RETURNING *
-// .then(result => {
-//     //reject promise if :review_id is not found (result array is empty)
-//     if (!result.rows.length) {
-//         return Promise.reject({status: 404, msg: 'ID Not Found'});
-//     }
-//     return result.rows[0];
-// })
