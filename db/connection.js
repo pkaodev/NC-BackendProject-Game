8717b75328b960275956f4bfb0d4d7d663095b7d
pkaodev <polsson.development@gmail.com>
2022-05-19 14:04:49 +0100

    commented out env config

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
-const path = require('path')
+
 //set node environment variable
 const ENV = process.env.NODE_ENV || 'development';
 
 //configure environment from env file
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
 
 //throw error if neither test/dev PGDATABASE or prod DATABASE_URL are set
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
